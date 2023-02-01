6892afa7c2ffbc3b5e7510c31d56896737bf56ba
cstuser <nadiohos@icloud.com>
2023-02-01 10:34:44 -0500

    Add

--- eCommerce/Views/Main/index.php
+++ eCommerce/Views/Main/index.php
@@ -8,7 +8,10 @@
 <body>
 	<h1>Welcome!</h1>
 	<p>Welcome to this application</p>
+	<a href='/Main/index'>This is the way we want to access Index</a>
 	<a href='/index.php?url=/Main/index'>Index</a>
+
+	
 	<a href='/index.php?url=/Main/index2'>Index2</a>
 	<a href='/index.php?url=/Main/Greetings/Bob'>Greet Bob</a>
 </body>
