044c6c1b96400a249f628163d2aee1f0f3aef9bb
cstuser <nadiohos@icloud.com>
2023-02-07 11:46:35 -0500

    Add

--- app/views/shared/footer.php
+++ app/views/shared/footer.php
@@ -1,6 +1,9 @@
-	<a href='/Main/logUser'>Write your name in the log book</a>
-	<a href='/Main/index'>Index</a>
-	<a href='/Main/index2'>Index2</a>
-	<a href='/Main/Greetings/Bob'>Greet Bob</a>
+<ul>
+	<li><a href='/Main/logUser'>Write your name in the log book</a></li>
+	<li><a href='/Main/viewUserLog'>View the log book</a></li>
+	<li><a href='/Main/index'>Index</a></li>
+	<li><a href='/Main/index2'>Index2</a></li>
+	<li><a href='/Main/Greetings/Bob'>Greet Bob</a></li>
+</ul>
 </body>
 </html>
