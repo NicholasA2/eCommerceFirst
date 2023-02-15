7b0180500bfdda5436bbe69bf137b331942da4a4
cstuser <nadiohos@icloud.com>
2023-02-15 11:37:29 -0500

    add

---
+++ app/views/User/profile.php
@@ -0,0 +1,6 @@
+<?php $this->view('shared/header', 'Log into your account'); ?>
+
+USER PROFILE!!!
+<a href='User/logout'>Logout</a>
+
+<?php $this->view('shared/footer'); ?>
--- app/views/shared/footer.php
+++ app/views/shared/footer.php
@@ -4,6 +4,9 @@
 	<li><a href='/Main/index'>Index</a></li>
 	<li><a href='/Main/index2'>Index2</a></li>
 	<li><a href='/Main/Greetings/Bob'>Greet Bob</a></li>
+	<li><a href='/User/index'>Secure application</a></li>
+	<li><a href='/User/profile'>User profile</a></li>
 </ul>
+</div>
 </body>
 </html>
