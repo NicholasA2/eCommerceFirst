f24210a178bbc49fd9bc09c6ae280a04373724e8
cstuser <nadiohos@icloud.com>
2023-04-04 11:47:47 -0400

    i18n

--- app/views/shared/footer.php
+++ app/views/shared/footer.php
@@ -6,6 +6,10 @@
 	<li><a href='/Main/Greetings/Bob'>Greet Bob</a></li>
 	<li><a href='/User/index'>Secure application</a></li>
 	<li><a href='/User/profile'>User profile</a></li>
+	<li><a href='?lang=fr_CA'>Francais (Canada)</a></li>
+	<li><a href='?lang=en'>English</a></li>
+	<li><a href='?lang=ar'>عربي</a></li>
+	<!--li><a href='#top'>Top</a></li-->
 </ul>
 </div>
 </body>
