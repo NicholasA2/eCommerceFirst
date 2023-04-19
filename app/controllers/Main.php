45ee6226f194e2d3973a5a2f4e6c65f68133f6d0
cstuser <nadiohos@icloud.com>
2023-04-19 09:32:50 -0400

    2fa

--- app/controllers/Main.php
+++ app/controllers/Main.php
@@ -40,5 +40,41 @@ class Main extends \app\core\Controller{
 		header('location:/Main/viewUserLog');//redirect
 	}
 
+	// Use: /Default/makeQRCode?data=protocol://address
+	public function makeQRCode(){
+		$data = $_GET['data'];
+		\QRcode::png($data);
+	}
+
+	#[\app\filters\Login]
+	public function setup2fa(){
+		 if(isset($_POST['action'])){
+		 $currentcode = $_POST['currentCode'];
+		 if(\app\core\TokenAuth6238::verify(
+		 $_SESSION['secretkey'],$currentcode)){
+		//the user has verified their proper 2-factor authentication
+		setup
+		 $user = new \App\models\User();
+		 $user->user_id = $_SESSION['user_id'];
+		 $user->secret_key = $_SESSION['secretkey'];
+		 $user->update2fa();
+		 header('location:'.BASE.'/Somewhere***');
+		 }else{
+		 header('location:'.BASE.'/Main/setup2fa?error=token
+		not verified!');//reload
+		 }
+		 }else{
+		 $secretkey =
+		\app\core\TokenAuth6238::generateRandomClue();
+		 $_SESSION['secretkey'] = $secretkey;
+		 $url = \App\core\TokenAuth6238::getLocalCodeUrl(
+		 $_SESSION['username'],
+		 'Awesome.com',
+		 $secretkey,
+		 'Awesome App');
+		 $this->view('Main/twofasetup', $url);
+ 	}
+}
+
 
 }
--- app/core/init.php
+++ app/core/init.php
@@ -3,3 +3,4 @@ session_start();
 include 'app/core/i18n.php';
 include 'app/core/autoload.php';
 include 'vendor/autoload.php';
+require ("core/phpqrcode/qrlib.php");
